78ee6ea6b9326b79025922703bcd39308de8a0de
Ethan Munsami <ethanmunsami@gmail.com>
2023-06-07 06:44:27 -0700

    Added 1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+
+	s++;
+	_print_rev_recursion(s);
+	s--;
+	_putchar(s);
+}
