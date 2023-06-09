c13c06370b3bb0e7cb006dd8e969141f625029c3
ethanmunsami <125829979+ethanmunsami@users.noreply.github.com>
2023-06-09 23:22:37 +0200

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+* main - Function prints its name
+* 
+* @argc: Argument count
+* @argv: Argument vector
+*
+* Return: (0)
+*/
+
 int main(int argc, char *argv[])
 {
                 printf("%s\n", argv[0]);
+  return (0);
 }
