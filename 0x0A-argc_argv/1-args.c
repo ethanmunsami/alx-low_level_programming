77e63129767d0b65a1110410762215469d0d10e3
Ethan Munsami <ethanmunsami@gmail.com>
2023-06-09 14:31:35 -0700

    Added files

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * main - Prints the number of arguments
+  *
+  * @argc: Argument count
+  * @argv: Argument vector
+  *
+  * Return: (0)
+  */
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
