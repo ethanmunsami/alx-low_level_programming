e248ef3934080bfcb3d146d09903fba99f309301
ethanmunsami <125829979+ethanmunsami@users.noreply.github.com>
2023-06-13 19:39:32 +0200

    Updated 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -19,7 +19,7 @@ char *str_concat(char *s1, char *s2)
 	if (s1 == NULL)
 		s1 = "";
 
-	if s2 == (NULL)
+	if (s2 == NULL)
 		s2 = "";
 
 	while (s1[a])
@@ -48,7 +48,7 @@ char *str_concat(char *s1, char *s2)
 
 		if (c >= a)
 		{
-			s[c] = s2[b];
+			x[c] = s2[b];
 			b++;
 		}
 
