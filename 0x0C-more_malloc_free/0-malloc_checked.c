1b1a3e772d21ee1e8ae1d316d9076e5d9de379a2
Ethan Munsami <ethanmunsami@gmail.com>
2023-06-16 08:56:55 -0700

    Added files

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * malloc_checked - Function allocates memory
+ * 
+ * @b: The number of arguments
+ *
+ * Return: A pointer to the allocated memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *x;
+
+	x = malloc(sizeof(b));
+
+	if (b == NULL)
+		return (98);
+
+	return (x);
+}
