cc3cadea57b4b1bbd9a805ccfa8542cc948d8ddc
Ethan Munsami <ethanmunsami@gmail.com>
2023-06-19 12:25:14 -0700

    Updated dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,3 +1,16 @@
+#ifndef _HEADER_
+#define _HEADER_
+
+/**
+  * struct dog - Dog attributes
+  *
+  * @name: The name of the dog
+  * @age: The age of the dog
+  * @owner: The owner of the dog
+  *
+  * Description: The attributes of a dog
+  */
+
 struct dog
 {
 	char *name;
