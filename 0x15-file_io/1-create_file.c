419166d1e4a600313394951b291af60eead8d42f
Ethan Munsami <ethanmunsami@gmail.com>
2023-09-04 23:31:09 +0200

    Added files

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+  * create_file - Function creates a file
+  *
+  * @filename: The source file
+  * @text_content: A NULL terminated string to write to the file
+  *
+  * Return: 1 on success, -1 on failure
+  */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content)
+		write(fd, text_content, _strlen(text_content));
+
+	close(fd);
+	return (1);
+}
+
+/**
+  * _strlen - Function returns the length of a string
+  *
+  * @s: String to count
+  *
+  * Return: The length of the string
+  */
+
+int _strlen(char *s)
+{
+	int c = 0;
+
+	while (s[c])
+		c++;
+
+	return (c);
+}
